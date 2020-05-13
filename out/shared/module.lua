8489926a0e00004db1b1e32d499127eba911ddcc
abhashs <galeant222@gmail.com>
2020-05-13 11:16:35 -0700

    Initial commit

---
+++ out/shared/module.lua
@@ -0,0 +1,9 @@
+-- Compiled with https://roblox-ts.github.io v0.3.2
+-- May 13, 2020, 11:00 AM Mountain Standard Time
+
+local exports = {};
+local function makeHello(name)
+	return "Hello from " .. name .. "!";
+end;
+exports.makeHello = makeHello;
+return exports;
